959b1f9b1aeffcc5120e04fe20f638ef091ccb2d
antvasanth <126975259+antvasanth@users.noreply.github.com>
2023-03-23 17:49:01 +0530

    second commit

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -41,6 +41,44 @@ class CommUtil {
       style: GoogleFonts.outfit(color: col, fontSize: fs.sp, fontWeight: fw),
     );
   }
+
+  Widget bot(String img, String val, Color col) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Card(
+        elevation: 10,
+        color: const Color(0xFF2E2C71),
+        child: Container(
+          height: 28.h,
+          width: 28.w,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.w), color: col),
+          child: Image.asset(
+            img,
+            height: 16.h,
+            width: 16.w,
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 6.h,
+      ),
+      commUtil.txt(val, Colors.white, 12, FontWeight.w400)
+    ],
+  );
+}
+
+Widget sideDrawer(String name) {
+  return Padding(
+    padding: EdgeInsets.only(bottom: 10.h),
+    child: ListTile(
+      title: commUtil.txt(name, Colors.white, 16, FontWeight.w600),
+      tileColor: const Color(0xFF1B2152),
+    ),
+  );
+}
+
 }
 
 CommUtil commUtil = CommUtil();
